72b16cac828aa8abee866dbda6f597dfdd417b6d
Chloe Steffens <caforonda103@yahoo.com>
2020-09-07 01:05:33 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get 'products/index'
 
   get 'products/add'
+  
+  get '/', to: 'products#index', as: :products
+
+  post '/add', to: 'products#add', as: :add_product
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
